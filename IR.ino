9dec253a0ded633d00c8d423f169043de00be548
ElectricBeat <96882916+ElectricBeat@users.noreply.github.com>
2022-03-09 07:18:02 +0530

    LYCASOFT
    LYCASOFT TRINNING CODE FOR ARDUINO UNO IR DETECTION

---
+++ IR.ino
@@ -0,0 +1,39 @@
+#include <LiquidCrystal.h> 
+LiquidCrystal lcd(0,1,2, 3, 4, 5);
+
+const int LED=13;
+const int SW=9;
+
+int val=0;
+int once=0;
+
+void setup()
+{
+  lcd.begin(16, 2);
+    
+  lcd.setCursor(0,0); 
+  lcd.print("SRI ELECTRONIC & EMBEDDED SOLUTIONS");
+  delay(1000);
+      
+  pinMode(LED,OUTPUT);
+  pinMode(SW,INPUT);
+}
+
+void loop()
+{
+  val=digitalRead(SW);
+  delay(120);
+  
+  if(val==HIGH)
+  {
+    digitalWrite(LED,HIGH);
+    lcd.setCursor(0,1); 
+    lcd.print("OBJECT DEDUCTED");
+  }
+  else 
+  {
+    digitalWrite(LED,LOW);
+    lcd.setCursor(0,1); 
+    lcd.print("OBJECT SERCHING");
+  }
+}
